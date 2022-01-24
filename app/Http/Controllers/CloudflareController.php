c927d8306b9ebc44ac20b266bb720f546f255048
Rafael Vieira <rafaeldevcode@gmail.com>
2022-01-24 16:50:19 -0300

    Teste-19

--- app/Http/Controllers/CloudflareController.php
+++ app/Http/Controllers/CloudflareController.php
@@ -37,7 +37,7 @@ class CloudflareController extends Controller
 
         $response = $conectar->getApiCloudflare($conta, 'zones');
         $response = json_decode($response, true);
-        dd($response);
+        dd($response['result'][0]['name']);
         // foreach($response as $item){
         //     echo $item;
         // }
