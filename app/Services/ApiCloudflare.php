90f8ba5a60659b6a2af72acc49ece91994ebe9ca
Rafael Vieira <rafaeldevcode@gmail.com>
2022-01-25 09:22:27 -0300

    Adicionando classe css no formulario de limpar cache - Teste-4

--- app/Services/ApiCloudflare.php
+++ app/Services/ApiCloudflare.php
@@ -21,11 +21,11 @@ class ApiCloudflare{
         $reponse = Http::withHeaders([
             'X-Auth-Key'   => $conta->chave_api,
             'X-Auth-Email' => $conta->email,
-            'Content-Type' => 'application/json',
-            '--data'         => [
+            // 'Content-Type' => 'application/json',
+            'data'         => [
                 'purge_everything' => true
             ]
-        ])->post("https://api.cloudflare.com/client/v4/{$id}/purge_cache/");
+        ])->get("https://api.cloudflare.com/client/v4/{$id}/purge_cache/");
 
         return $reponse;
     }
