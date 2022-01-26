58ea0fbe998423130de0c4037cbabe28a82b524a
Rafael Vieira <rafaeldevcode@gmail.com>
2022-01-26 16:39:32 -0300

    Fazendo teste para limpar por tag - Teste-6

--- app/Services/ApiCloudflare.php
+++ app/Services/ApiCloudflare.php
@@ -84,8 +84,8 @@ class ApiCloudflare{
                 'X-Auth-Key'   => $conta->chave_api,
                 'X-Auth-Email' => $conta->email,
                 'Content-Type' => 'application/json'
-            ])->get("https://api.cloudflare.com/client/v4/zones/?page={$total_pages[$i]}")['result'];
-            array_push($responses, $response[$i]);
+            ])->get("https://api.cloudflare.com/client/v4/zones/?page={$total_pages[$i]}")['result'][$i]['name'];
+            array_push($responses, $response);
         }
 
         return $responses;
