0d11814e1166c828534614c3357acdb45715aa09
Rafael Vieira <rafaeldevcode@gmail.com>
2022-01-27 17:46:53 -0300

    Fazendo teste para limpar por tag - Teste-78

--- resources/views/layouts/componentes/tags.blade.php
+++ resources/views/layouts/componentes/tags.blade.php
@@ -3,7 +3,7 @@
     @for ($i = 0; $i < count($resultados['dominios']); $i++)
         <span class="d-flex p-1 border rounded">
             <label class="form-check-label fs-6" for="dominio-{{ $i }}">{{ $resultados['dominios'][$i] }}</label>
-            <input class="form-check-input ms-1" id="dominio-{{ $i }}" type="checkbox" name="ids_dominio[]" value="{{ $resultados['ids_cloudflare'][$i] }}">
+            <input class="form-check-input ms-1" id="dominio-{{ $i }}" type="checkbox" name="ids_dominio[]" value="{{ $resultados['ids_dominio'][$i] }}">
         </span>
     @endfor
 </div>
