1860cf696dc4abb3bcc3aa420091cfdb063e0280
Rafael Vieira <rafaeldevcode@gmail.com>
2022-01-27 15:06:20 -0300

    Fazendo teste para limpar por tag - Teste-45

--- resources/views/painel/cloudflare/adicionarTag.blade.php
+++ resources/views/painel/cloudflare/adicionarTag.blade.php
@@ -25,7 +25,7 @@
 
                 <section class="container-fluid">
                     @foreach ($response as $item)
-                        <p>{{$item[0]}}</p>
+                        {{ print_r($item) }}
                     @endforeach
                 </section>
             </div>
