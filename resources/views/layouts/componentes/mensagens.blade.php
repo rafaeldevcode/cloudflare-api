317a100c8fd3dfb0b7f0ba767a5cfce43368ea42
Rafael Vieira <rafaeldevcode@gmail.com>
2022-01-28 10:44:43 -0300

    Fazendo teste para limpar por tag - Teste-102

--- resources/views/layouts/componentes/mensagens.blade.php
+++ resources/views/layouts/componentes/mensagens.blade.php
@@ -1,8 +1,9 @@
 @if (!empty($mensagens))
+    <span class="col-12 d-flex justify-content-end">
+        <i class="fas fa-times-circle fs-6 text-cloudflare" id="remover"></i>
+    </span>
+    
     @foreach ($mensagens as $mensagem)
-        <span class="col-12 d-flex justify-content-end">
-            <i class="fas fa-times-circle fs-6 text-cloudflare" id="remover"></i>
-        </span>
         <div class="alert alert-success">
             {{ $mensagem }}
         </div>
