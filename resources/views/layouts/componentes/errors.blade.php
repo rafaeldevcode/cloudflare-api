71e76b8a31a55ed8abcb85b25327404e9e3251f4
Rafael Vieira <rafaeldevcode@gmail.com>
2022-01-28 11:16:56 -0300

    Fazendo teste para limpar por tag - Teste-106

--- resources/views/layouts/componentes/errors.blade.php
+++ resources/views/layouts/componentes/errors.blade.php
@@ -6,7 +6,7 @@
 
         <ul class="p-0">
             @foreach ($errors->all() as $error)
-                <li class="p-1 my-1 alert alert-danger">{{ $error }}</li>
+                <li class="px-3 my-1 alert alert-danger">{{ $error }}</li>
             @endforeach
         </ul>
     </div>
